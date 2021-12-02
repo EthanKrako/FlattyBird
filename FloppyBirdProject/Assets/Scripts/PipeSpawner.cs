2e7c1ad10a22ece9d7be3839a566a9906403f3bd
EthanKrako <ethankrakowski33@gmail.com>
2021-12-02 19:02:33 +0100

    PipeSpawner
    Spawn pipes at regular intervals

---
+++ FloppyBirdProject/Assets/Scripts/PipeSpawner.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PipeSpawner : MonoBehaviour
+{
+    public GameObject pipe;
+    private bool isPlaying = true;
+    public float delai;
+    private float temps;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        temps = delai;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        print(Time.deltaTime);
+        temps -= Time.deltaTime;
+        if(isPlaying && (temps < 0)) {
+            Instantiate(pipe, this.transform);
+            temps += delai;
+        }
+    }
+}
