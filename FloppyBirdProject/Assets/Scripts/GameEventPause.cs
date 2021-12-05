5c1eee545124541531594fb7c629e2d9403e6beb
EthanKrako <ethankrakowski33@gmail.com>
2021-12-05 16:06:17 +0100

    Added pause
    Game pause itself when pressing escape

---
+++ FloppyBirdProject/Assets/Scripts/GameEventPause.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameEventPause : MonoBehaviour
+{
+    public GameObject pauseMenu;
+    // Start is called before the first frame update
+    private void Start() {
+        pauseMenu.SetActive(false);
+    }
+
+    private void Update() {
+        if (Input.GetButtonDown("Pause")) {
+            if (!pauseMenu.activeSelf) {
+                PauseGame();
+            } else {
+                ResumeGame();
+            }
+        }
+    }
+
+    private void PauseGame() {
+        pauseMenu.SetActive(true);
+        Time.timeScale = 0;
+    }
+
+    private void ResumeGame() {
+        pauseMenu.SetActive(false);
+        Time.timeScale = 1;
+    }
+}
